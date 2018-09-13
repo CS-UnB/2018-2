255825676e5ebe6397bc9b98fbfb4a11ac96f7e9
Otto <ottok92@gmail.com>
2018-09-13 19:29:29 +0000

    Concurrent Programming: Estudo Dirigido 4 - Done

--- ConcurrentProgramming/EstudoDirigido/canibais.c
+++ ConcurrentProgramming/EstudoDirigido/canibais.c
@@ -1,5 +1,3 @@
-//CONTINUE FROM MAIN
-
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,16 +17,16 @@ void* chef(void* arg){
 	while(1){
 		pthread_mutex_lock(&mutex);
 		while(food != 0){
-			printf("CHEF %d: WAITING. These bastards still have food.", id);
+			printf("CHEF %d: WAITING. These bastards still have food.\n", id);
 			pthread_cond_wait(&cond_chef, &mutex);
 		}
 		// cooking
-		sleep(5);
-		printf("CHEF %d: COOKING. I'd better cook or else I'll be the dinner!", id);
+		//sleep(5);
+		printf("CHEF %d: COOKING. I'd better cook or else I'll be the dinner!\n", id);
 		food += SERVING;
 		// serving
 		pthread_cond_broadcast(&cond_cannibals);
-		printf("CHEF %d: SERVING. Wake up, bastards. Time for food!", id);
+		printf("CHEF %d: SERVING. Wake up, bastards. Time for food!\n", id);
 		pthread_mutex_unlock(&mutex);
 	}
 }
@@ -38,33 +36,42 @@ void* cannibal(void* arg){
 	while(1){
 		pthread_mutex_lock(&mutex);
 		while(food == 0){
-			printf("Cannibal %d: NO FOOD. Let's wait..", id);
+			printf("Cannibal %d: NO FOOD. Let's wait..\n", id);
 			pthread_cond_wait(&cond_cannibals, &mutex);
 		}
 		// getting the food
-		printf("Cannibal %d: GETTING food", id);
+		printf("Cannibal %d: GETTING food\n", id);
 		food--;
 		if(food == 0){
-			printf("Cannibal %d: WAKE THE CHEF. We need more food!", id);
+			printf("Cannibal %d: WAKE THE CHEF. We need more food!\n", id);
 			pthread_cond_signal(&cond_chef);
 		}
 		pthread_mutex_unlock(&mutex);
 		// eating
-		printf("Cannibal %d: EATING... good food, the chef is safe for now.", id);
-		sleep(10);
+		printf("Cannibal %d: EATING... good food, the chef is safe for now.\n", id);
+		//sleep(10);
 	}
 }
 
 int main(int argc, char** argv){
-	
+	pthread_t store_cannibals[CANNIBALS];
+    pthread_t store_chef;
 	int *id;
 	int i = 0;
 
-	pthread_create(NULL, NULL, &chef, (void*) id);
-	for(i = 0; i < CANNIBALS; ++i){
-		
+    id = (int *) malloc(sizeof(int));
+    *id = 1;
+	pthread_create(&store_chef, NULL, chef, (void*) id);
+    // create cannibals threadsa
+    for(i = 0; i < CANNIBALS; ++i){
+	    *id = i;
+        pthread_create(&store_cannibals[i], NULL, cannibal, (void*) id);
 	}
-        id = (int *) malloc(sizeof(int));
-        *id = i;
+    
+    for(i = 0; i < CANNIBALS; ++i){
+	    *id = i;
+        pthread_create(&store_cannibals[i], NULL, cannibal, (void*) id);
+	}
+    pthread_join(store_cannibals[0], NULL);
 	return 0;
 }
