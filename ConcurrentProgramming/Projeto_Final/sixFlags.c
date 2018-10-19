cb2c30ce777521c7247307b27df8cb7b54a96d91
Otto-Avell <ottok92@gmail.com>
2018-10-19 13:09:01 -0300

    Final_Project: Threads prototypes created.

---
+++ ConcurrentProgramming/Projeto_Final/sixFlags.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+void * toddler(void *arg) {}
+void * child(void *arg) {}
+void * teen(void *arg) {}
+void * youngAdult(void *arg) {}
+void * middleAgedAdult(void *arg) {}
+void * elder(void *arg) {}
